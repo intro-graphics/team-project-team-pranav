bbdfa47b7b28e58d6f31643543ea7ae093c5add9
articbear1999 <jacobchenlin@gmail.com>
2020-05-13 16:52:06 -0700

    change camera movement to ijkl

--- dependencies.js
+++ dependencies.js
@@ -354,10 +354,10 @@ class Movement_Controls extends Scene_Component    // Movement_Controls is a Sce
     { const globals = this.globals;
       this.control_panel.innerHTML += "Click and drag the scene to <br> spin your viewpoint around it.<br>";
       this.key_triggered_button( "Up",     [ " " ], () => this.thrust[1] = -1, undefined, () => this.thrust[1] = 0 );
-      this.key_triggered_button( "Forward",[ "w" ], () => this.thrust[2] =  1, undefined, () => this.thrust[2] = 0 );  this.new_line();
-      this.key_triggered_button( "Left",   [ "a" ], () => this.thrust[0] =  1, undefined, () => this.thrust[0] = 0 );
-      this.key_triggered_button( "Back",   [ "s" ], () => this.thrust[2] = -1, undefined, () => this.thrust[2] = 0 );
-      this.key_triggered_button( "Right",  [ "d" ], () => this.thrust[0] = -1, undefined, () => this.thrust[0] = 0 );  this.new_line();
+      this.key_triggered_button( "Forward",[ "i" ], () => this.thrust[2] =  1, undefined, () => this.thrust[2] = 0 );  this.new_line();
+      this.key_triggered_button( "Left",   [ "j" ], () => this.thrust[0] =  1, undefined, () => this.thrust[0] = 0 );
+      this.key_triggered_button( "Back",   [ "k" ], () => this.thrust[2] = -1, undefined, () => this.thrust[2] = 0 );
+      this.key_triggered_button( "Right",  [ "l" ], () => this.thrust[0] = -1, undefined, () => this.thrust[0] = 0 );  this.new_line();
       this.key_triggered_button( "Down",   [ "z" ], () => this.thrust[1] =  1, undefined, () => this.thrust[1] = 0 ); 
 
       const speed_controls = this.control_panel.appendChild( document.createElement( "span" ) );
