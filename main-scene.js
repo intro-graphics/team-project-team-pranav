8f21be3e97ad65430f0c85618a9ad701ae829db4
articbear1999 <jacobchenlin@gmail.com>
2020-05-13 16:46:24 -0700

    put the folder outside

---
+++ main-scene.js
@@ -0,0 +1,145 @@
+// cube definition from Assignment 2, can be moved into dependencies.js to improve readability
+window.Cube = window.classes.Cube =
+    class Cube extends Shape {
+        // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
+        constructor() {
+            super("positions", "normals"); // Name the values we'll define per each vertex.  They'll have positions and normals.
+
+            // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
+            this.positions.push(...Vec.cast(
+                [-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1],
+                [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1],
+                [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]));
+            // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+            // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+            this.normals.push(...Vec.cast(
+                [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+                [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+                [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]));
+
+            // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+            // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+            // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+            this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+                14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+            // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
+        }
+    }
+
+
+window.Shadow_Demo = window.classes.Shadow_Demo =
+class Shadow_Demo extends Scene_Component
+  { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
+      { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
+        if( !context.globals.has_controls   ) 
+          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
+
+        context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,10,20 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
+        this.initial_camera_location = Mat4.inverse( context.globals.graphics_state.camera_transform );
+
+        this.ud = 0;  // the up-down position of the character
+        this.lr = 0; // the left-right position of the character
+
+        const r = context.width/context.height;
+        context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
+        
+        const shapes = { // Shapes from Assignment Three
+                         torus:  new Torus( 15, 15 ),
+                         torus2: new ( Torus.prototype.make_flat_shaded_version() )( 15, 15 ),
+                         sub1: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(1),
+                         sub2: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
+                         sub3: new (Subdivision_Sphere)(3),
+
+                         // Shapes used so far                     
+                         sub4: new (Subdivision_Sphere)(4), 
+                         body: new (Cube)()
+                       }
+        this.submit_shapes( context, shapes );
+        this.materials =
+          { 
+            // the only material used so far
+            suns: context.get_instance( Phong_Shader ).material( Color.of(1, 0, 0, 1), 
+            { 
+              ambient: 1, specularity: 0
+            }),
+
+            // materials from Assignment Three
+            planets: context.get_instance( Phong_Shader ).material(Color.of(0, 0, 0, 1)),
+            test: context.get_instance( Phong_Shader ).material( Color.of(0, 0, 1, 1), { ambient: 0.2 } )
+          }
+
+        this.lights = [ new Light( Vec.of( 5,-10,5,1 ), Color.of( 0, 1, 1, 1 ), 1000 ) ];
+      }
+    make_control_panel()            // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
+      { 
+        this.key_triggered_button("Up", ["w"], () => {  // going Up with 'i'
+                if(this.ud >= -96.8)  // 485 steps max upwards (from starting position) (not 484 because >=)
+                  this.ud = this.ud - 0.2;
+            });
+        this.key_triggered_button("Down", ["s"], () => {  // going Down with 'k'
+                if(this.ud <= 4.6)  // 24 steps max downwards
+                  this.ud = this.ud + 0.2;
+            });
+        this.key_triggered_button("Left", ["a"], () => {  // going Left with 'j'
+                if(this.lr >= -10.4)  //  53 steps max left
+                  this.lr = this.lr - 0.2;
+            });
+        this.key_triggered_button("Right", ["d"], () => { // going right with 'l'
+                if(this.lr <= 10.4) //  53 steps max right
+                  this.lr = this.lr + 0.2;
+            });
+      }
+    display( graphics_state )
+      {        
+        graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
+        const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
+
+        // our position matrix
+        let pos = Mat4.identity();
+
+        // the road
+        pos = pos.times(Mat4.scale([3.5,5,100])).times(Mat4.rotation(Math.PI / 2, Vec.of(0, 1, 0))).times(Mat4.translation([0, 0, 0]));
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0.5, 0.5, 0.5, 1)} ));
+
+        // the grass to the right
+        pos = pos.times(Mat4.scale([1,1,8])).times(Mat4.translation([0, 0, 1.125]))
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0.5, 1, 0.5, 1)} ));
+
+        // the grass to the left
+        pos = pos.times(Mat4.translation([0, 0, -2.25]))
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0.5, 1, 0.5, 1)} ));
+
+        // the cave
+        pos = Mat4.identity().times(Mat4.scale([3.5,3.2,3])).times(Mat4.translation([0,1.5,-6]));
+        this.shapes.sub4.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0.5, 0.25, 0, 1)} ));
+        
+        //the cave entrance
+        pos = Mat4.identity().times(Mat4.scale([2.5,3,3])).times(Mat4.translation([0,1.3,-5.6]));
+        this.shapes.sub4.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0, 0, 0, 1)} ));
+
+        // the blue building
+        pos = Mat4.identity().times(Mat4.translation([6,5,0])).times(Mat4.scale([1.5,2,2]));
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0.25, 0.9, 1, 1)} ));
+
+        //  the yellow building
+        pos = pos.times(Mat4.translation([-8,0,4]))
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(1, 1, 0.5, 1)} ));
+
+        // the gray building
+        pos = pos.times(Mat4.translation([8,0,2]))
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0.5, 0.5, 0.5, 1)} ));
+
+        // the skybox (can be seen if you tilt the camera)
+        pos = Mat4.identity().times(Mat4.scale([100,100,100]))
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(0.25, 0.9, 1, 1)} ));
+
+        // the character
+        pos = Mat4.identity().times(Mat4.translation([0,5.5,14])).times(Mat4.scale([0.3,0.5,0.3])).times(Mat4.translation([this.lr,0,this.ud]))
+        this.shapes.body.draw(graphics_state, pos, this.materials.suns.override( {color: Color.of(1, 0, 0, 1)} ));
+
+        pos = Mat4.identity()
+
+        // you guys can start from here
+
+      }
+  }
